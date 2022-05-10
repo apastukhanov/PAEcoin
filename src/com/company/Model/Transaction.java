627cca5a0e49f07f042578b5e36ae7f0bf762e48
aleksandr <apastukhanov@mail.ru>
2022-05-11 00:41:02 +0300

    added fields to Transaction class

--- src/com/company/Model/Transaction.java
+++ src/com/company/Model/Transaction.java
@@ -11,4 +11,15 @@ import java.util.Arrays;
 import java.util.Base64;
 
 public class Transaction implements Serializable{
+
+    private byte[] from;
+    private String fromFX;
+    private byte[] to;
+    private String toFX;
+    private Integer value;
+    private String timestamp;
+    private byte[] signature;
+    private String signatureFX;
+    private Integer ledgerId;
+
 }
